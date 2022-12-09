f272645230c107d49ef0ed9771e263873b4af7c5
dsuclassprojects <savannah.walsdorf@trojans.dsu.edu>
2022-12-08 20:13:32 -0600

    Testing again sorry

--- Cart.aspx.cs
+++ Cart.aspx.cs
@@ -11,12 +11,16 @@ namespace WebApplication2
     {
         public string Price { get; set; }
         public string Size { get; set; }
+
+        public string Crust { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
             string _price = Request.QueryString["Price"];
             string _size = Request.QueryString["Size"];
+            string _crust = Request.QueryString["Crust"];
             this.Price = _price;
             this.Size = _size;
+            this.Crust = _crust;
         }
     }
 }
